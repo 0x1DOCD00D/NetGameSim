968f733099f7a1f9da2e727b20bb794cf4fee780
Mark Grechanik <drmark@uic.edu>
2023-09-20 13:31:01 -0500

    fixed serialization file deletion

--- NetModelGenerator/src/test/scala/NetGraphAlgebraDefs/NetGraphTest.scala
+++ NetModelGenerator/src/test/scala/NetGraphAlgebraDefs/NetGraphTest.scala
@@ -5,6 +5,9 @@ import org.apache.commons.io.FileUtils
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.slf4j.Logger
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
 import NetGraphAlgebraDefs.NetGraph
 import NetGraphAlgebraDefs.NetModel
 import NetGraphAlgebraDefs.NetModelAlgebra
@@ -21,7 +24,10 @@ class NetGraphTest extends AnyFlatSpec with Matchers {
     val graph: NetGraph with GraphStore = NetModelAlgebra().get
     graph.persist(outputDirectory, "testGraph_1.ser")
     val res = FileUtils.getFile(outputDirectory + "testGraph_1.ser").exists()
-    FileUtils.forceDelete(FileUtils.getFile(outputDirectory + "testGraph_1.ser"))
+    Try(FileUtils.forceDelete(FileUtils.getFile(outputDirectory + "testGraph_1.ser"))) match
+       case Failure(exception) =>
+         logger.warn(s"Cannot delete file ${outputDirectory + "testGraph_1.ser"}")
+       case Success(value) => logger.info(s"Deleted file ${outputDirectory + "testGraph_1.ser"}")
     res shouldEqual true
   }
 
@@ -30,7 +36,10 @@ class NetGraphTest extends AnyFlatSpec with Matchers {
     graph.persist(outputDirectory, "testGraph_2.ser")
     FileUtils.getFile(outputDirectory + "testGraph_2.ser").exists() shouldEqual true
     val graph2 = NetGraph.load("testGraph_2.ser", outputDirectory)
-    FileUtils.forceDelete(FileUtils.getFile(outputDirectory + "testGraph_2.ser"))
+    Try(FileUtils.forceDelete(FileUtils.getFile(outputDirectory + "testGraph_2.ser"))) match
+       case Failure(exception) =>
+         logger.warn(s"Cannot delete file ${outputDirectory + "testGraph_2.ser"}")
+       case Success(value) => logger.info(s"Deleted file ${outputDirectory + "testGraph_2.ser"}")
     if graph2.isEmpty then assert(true, "Serialized graph not loaded")
     else graph2.get shouldEqual graph
   }
