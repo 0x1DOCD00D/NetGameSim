331e22b46e872486da104ca076a083f0b88b4694
swetha gumpena <swetha.gumpena0607@gmail.com>
2023-11-13 18:42:10 -0600

    fix: indentation

--- NetModelGenerator/src/main/scala/NetGraphAlgebraDefs/GraphStore.scala
+++ NetModelGenerator/src/main/scala/NetGraphAlgebraDefs/GraphStore.scala
@@ -26,19 +26,18 @@ trait GraphStore:
     val config = ConfigFactory.load()
     val outputGraphRepresentation = config.getConfig("NGSimulator").getConfig("OutputGraphRepresentation").getString("contentType")
     if (outputGraphRepresentation == "json") then
-      Try {
-        val nodesInGraph: String = sm.nodes().asScala.asJson.noSpaces
-        val edgesInGraph: String = sm.edges().asScala.toList.map { edge =>
-          sm.edgeValue(edge.source(), edge.target()).get
-        }.asJson.noSpaces
+        Try {
+          val nodesInGraph: String = sm.nodes().asScala.asJson.noSpaces
+          val edgesInGraph: String = sm.edges().asScala.toList.map { edge =>
+            sm.edgeValue(edge.source(), edge.target()).get
+          }.asJson.noSpaces
 
-        val file = new FileWriter(s"$dir$fileName")
-        file.write(nodesInGraph + "\n" + edgesInGraph)
-        file.close()
-
-      }.map(_ => NetGraph.logger.info(s"Successfully persisted the graph in json to $dir$fileName"))
-        .recover { case e => NetGraph.logger.error(s"Failed to persist the graph in json to $dir$fileName : ", e) }
-    else
+          val file = new FileWriter(s"$dir$fileName")
+          file.write(nodesInGraph + "\n" + edgesInGraph)
+          file.close()
+        }.map(_ => NetGraph.logger.info(s"Successfully persisted the graph in json to $dir$fileName"))
+          .recover { case e => NetGraph.logger.error(s"Failed to persist the graph in json to $dir$fileName : ", e) }
+    else {
       import java.io._
       import java.util.Base64
       import java.nio.charset.StandardCharsets.UTF_8
@@ -52,35 +51,36 @@ trait GraphStore:
           oos.close()
         }.map(_ => NetGraph.logger.info(s"Successfully persisted the graph to $dir$fileName"))
         .recover { case e => NetGraph.logger.error(s"Failed to persist the graph to $dir$fileName : ", e) }
+    }
 
-//  Use the following graphviz command to render the graph to an image:
-//  sfdp -x -Goverlap=scale -Tpng graph.dot > graph.png
-    def toDotVizFormat(name: String, dir: String = outputDirectory, fileName: String, outputImageFormat: Format = Format.DOT): Unit =
-      val nodes: List[NodeObject] = initState :: sm.nodes().asScala.toList
-      if nodes.count(_.id == 0) < 1 then
-        logger.error("The graph does not contain a start node with id 0")
-      else
-        val edges: List[Action] = sm.edges().asScala.toList.map { edge =>
-          sm.edgeValue(edge.source(), edge.target()).get
-        }.sortBy(_.fromNode.id)
-        val nodesMap = nodes.foldLeft(Map[Int, Node]()) { case (acc, nd) =>
-          acc + (nd.id -> (
-            if nd.id == 0 then
-              node(nd.id.toString).`with`(Color.RED).`with`(Label.markdown("**Init**"), Color.rgb("1020d0").font())
-            else
-              node(nd.id.toString)))
-        }
-        val linkedGraph = edges.foldLeft(nodesMap) { case (acc, edge) =>
-          if acc.contains(edge.fromNode.id) && acc.contains(edge.toNode.id) then
-            acc + (edge.fromNode.id -> acc(edge.fromNode.id).link(to(acc(edge.toNode.id)).`with`(weight(if (edge.cost*10).floor < 1 then 1 else (edge.cost*10).floor))))
+  //  Use the following graphviz command to render the graph to an image:
+  //  sfdp -x -Goverlap=scale -Tpng graph.dot > graph.png
+  def toDotVizFormat(name: String, dir: String = outputDirectory, fileName: String, outputImageFormat: Format = Format.DOT): Unit =
+    val nodes: List[NodeObject] = initState :: sm.nodes().asScala.toList
+    if nodes.count(_.id == 0) < 1 then
+      logger.error("The graph does not contain a start node with id 0")
+    else
+      val edges: List[Action] = sm.edges().asScala.toList.map { edge =>
+        sm.edgeValue(edge.source(), edge.target()).get
+      }.sortBy(_.fromNode.id)
+      val nodesMap = nodes.foldLeft(Map[Int, Node]()) { case (acc, nd) =>
+        acc + (nd.id -> (
+          if nd.id == 0 then
+            node(nd.id.toString).`with`(Color.RED).`with`(Label.markdown("**Init**"), Color.rgb("1020d0").font())
           else
-            logger.error(s"Edge $edge is not valid because it contains a node that is not in the graph")
-            acc
-        }
-        val g = graph(name).directed().`with`(linkedGraph.values.toList: _*).
-          linkAttr().`with` ("class", "link-class").`with`(linkedGraph.values.toList: _*)
-        Try(new GraphvizCmdLineEngine()).map(cmdlnEngine => cmdlnEngine.timeout(2, TimeUnit.MINUTES)).map { cmdlnEngine =>
+            node(nd.id.toString)))
+      }
+      val linkedGraph = edges.foldLeft(nodesMap) { case (acc, edge) =>
+        if acc.contains(edge.fromNode.id) && acc.contains(edge.toNode.id) then
+          acc + (edge.fromNode.id -> acc(edge.fromNode.id).link(to(acc(edge.toNode.id)).`with`(weight(if (edge.cost*10).floor < 1 then 1 else (edge.cost*10).floor))))
+        else
+          logger.error(s"Edge $edge is not valid because it contains a node that is not in the graph")
+          acc
+      }
+      val g = graph(name).directed().`with`(linkedGraph.values.toList: _*).
+        linkAttr().`with` ("class", "link-class").`with`(linkedGraph.values.toList: _*)
+      Try(new GraphvizCmdLineEngine()).map(cmdlnEngine => cmdlnEngine.timeout(2, TimeUnit.MINUTES)).map { cmdlnEngine =>
           Graphviz.useEngine(cmdlnEngine)
           Graphviz.fromGraph(g).render(Format.DOT).toFile(new File(s"$dir$fileName.${Format.DOT.fileExtension}"))
         }.map(_ => NetGraph.logger.info(s"Successfully rendered the graph to $dir$fileName.${outputImageFormat.fileExtension}"))
-          .recover { case e => NetGraph.logger.error(s"Failed to render the graph to $dir$fileName.${outputImageFormat.fileExtension} : ", e) }
+        .recover { case e => NetGraph.logger.error(s"Failed to render the graph to $dir$fileName.${outputImageFormat.fileExtension} : ", e) }
